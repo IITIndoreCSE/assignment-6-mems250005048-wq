70f3459f862a24b942d85c032d922082ee009fdf
mems250005048-wq <mems250005048@iiti.ac.in>
2025-09-12 17:04:16 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -25,7 +25,7 @@ cin>>Array2[i];
 int joinedsize = size1+size2;
 int JoinedArray[joinedsize];
 for (int i=0; i < size1 ;i++){
-JoinedArray[i] = Array[i];
+JoinedArray[i] = Array1[i];
 }
 for (int i=0; i < size2 ;i++){
 JoinedArray[size1 + i]=Array2[i]
